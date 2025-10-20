95adc00458efb3df3f9adc42403bf112f36b02c9
egcsullc <contact@egcsu.com>
2025-10-19 19:01:20 -0700

    fix(build): use next.config.mjs (and tailwind.config.js) for Vercel

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,6 @@
-import type { Config } from 'tailwindcss'
-export default {
+﻿/** @type {import('tailwindcss').Config} */
+module.exports = {
   content: ['./app/**/*.{ts,tsx}','./components/**/*.{ts,tsx}'],
   theme: { extend: {} },
   plugins: [],
-} satisfies Config
+};
